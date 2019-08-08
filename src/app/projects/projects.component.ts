ecca4ecfcdeb83bab9df6a14bd72d12397a2d567
dada-tech <daviddadaa@hotmail.com>
2019-08-08 13:14:46 -0600

    class query selecting working

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
+
+import {AfterViewInit, Directive, QueryList, ViewChildren} from '@angular/core';
+
 import {PostsService} from '../services/posts.service';
 import {Post} from '../home/home.component';
+
 
 @Component({
   selector: 'app-projects',
@@ -9,13 +13,18 @@ import {Post} from '../home/home.component';
 })
 
 export class ProjectsComponent implements OnInit {
-  posts: Array<Post>;
 
-  constructor(private postService: PostsService) { }
+  constructor(private postService: PostsService, private elRef: ElementRef) { }
+  posts: Array<Post>;
 
   ngOnInit() {
     this.loadPosts();
+    // console.log(this.elRef.nativeElement.querySelectorAll('.test'));
+    this.elRef.nativeElement.querySelector('.test').className = 'test2';
+    console.log(this.elRef.nativeElement.querySelector('.test'));
+    console.log(this.elRef.nativeElement.querySelector('.test2'));
   }
+
 
   loadPosts() {
     this.postService.loadPosts().subscribe(
