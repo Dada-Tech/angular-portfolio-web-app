624563ef775f62e33b7d1606c8a8e6ad1ffaf92c
dada-tech <daviddadaa@hotmail.com>
2019-08-26 10:48:29 -0600

    cloud background scroll ngIF route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dada-Portfolio';
+  constructor(private router: Router) { }
 }
