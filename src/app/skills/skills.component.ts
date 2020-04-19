1f920b0ceb57c2e4bb1647bc03b016b244375009
dada-tech <daviddadaa@hotmail.com>
2020-04-18 23:40:00 -0700

    loading spinner for skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FollowlinkService } from '../services/followlink.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-skills',
@@ -8,9 +9,12 @@ import { FollowlinkService } from '../services/followlink.service';
 })
 export class SkillsComponent implements OnInit {
 
-  constructor(public followlink: FollowlinkService) { }
+  constructor(public followlink: FollowlinkService, private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
+    this.spinner.show().catch(err => { console.error(err); });
+    setTimeout(() => {
+      this.spinner.hide().catch(err => { console.error(err); });
+    }, 1000);
   }
-
 }
